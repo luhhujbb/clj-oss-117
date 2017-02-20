a56545146359e9944b6f33da3876ca33af7bc77a
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2017-02-20 16:50:06 +0100

    [STRUCTURE] add structure for basic oss operation

--- project.clj
+++ project.clj
@@ -3,4 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+            :dependencies [[org.clojure/clojure "1.8.0"]
+                           [org.clojure/java.data "0.1.1"]
+                           ;; logging stuff
+                           [org.clojure/tools.logging "0.3.1"]
+                           [org.slf4j/slf4j-api "1.7.19"]
+                           [org.slf4j/slf4j-log4j12 "1.7.19"]
+                           [org.slf4j/jcl-over-slf4j "1.7.19"]
+                           [log4j "1.2.17"]
+                            ;;alicloud oss
+                           [com.aliyun.oss/aliyun-sdk-oss "2.4.0"]])
