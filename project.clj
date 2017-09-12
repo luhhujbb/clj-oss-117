9e900d6fbef32ae93557b9387d19f38cef196d0b
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2017-09-12 10:44:21 +0200

    [CLOJARS] first clojars release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject oss-117 "0.1.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject luhhujbb/oss-117 "0.1.1"
+  :description "Aliyun simple object storage client"
+  :url "https://github.com/luhhujbb/clj-oss-117"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.8.0"]
